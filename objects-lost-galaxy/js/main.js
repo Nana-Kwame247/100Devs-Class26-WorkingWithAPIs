e7d80ac2c93a30cd0fdd3af7c42669e880cadba3
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-05-06 04:13:09 +0300

    modified object lost galaxy with objects for pizza and solved the coding question

--- objects-lost-galaxy/js/main.js
+++ objects-lost-galaxy/js/main.js
@@ -1,1 +1,25 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {};
+
+//four properties
+
+pizza.make = "Dominos";
+pizza.type = "Peperoni";
+pizza.size = "medium";
+pizza.taste = "delicious";
+pizza.toppings = ["spinach", "onion", "pineapple"];
+
+// three methods
+
+pizaa.estimatedDeliveryTime = function () {
+  console.log(`Calculating`);
+};
+
+pizza.locationFinder = function () {
+  console.log(`Searching for the closest pizza location to you`);
+};
+
+pizza.constructorInput = function () {
+  console.log(`Choose the items you want on your pizza`);
+};
