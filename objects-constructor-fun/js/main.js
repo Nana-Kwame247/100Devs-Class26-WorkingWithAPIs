411f487c43c26cb4a7837e1a53678fdb2df1a1fc
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-05-06 08:25:38 +0300

    updated bonus constructor part

--- objects-constructor-fun/js/main.js
+++ objects-constructor-fun/js/main.js
@@ -1,1 +1,25 @@
 //Create a constructor with 4 properties and 3 methods
+
+function PizzaMachine(size, toppings, crust, sauce) {
+  this.size = size;
+  this.toppings = toppings;
+  this.crust = crust;
+  this.sauce = sauce;
+
+  this.estimatedDeliveryTime = function () {
+    console.log(`calculating`);
+  };
+  this.burnMouth = function () {
+    console.log(`jdjbv;eefvjjd`);
+  };
+  this.frisbee = function () {
+    console.log(`Yyyeeeettttt`);
+  };
+}
+
+let pizza = new PizzaMachine(
+  "large",
+  ["spinach", "garlic"],
+  "deep dish",
+  "white"
+);
